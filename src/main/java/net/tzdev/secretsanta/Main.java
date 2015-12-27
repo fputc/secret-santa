ed7afdb079febd0ee565d5b86eca0bad9d75e049
unknown <ahelac@nbtno021.dccsintra.net>
2015-12-27 19:49:37 +0100

    Initial commit

---
+++ src/main/java/net/tzdev/secretsanta/Main.java
@@ -0,0 +1,11 @@
+package net.tzdev.secretsanta;
+
+/**
+ * Created by ahelac on 27-Dec-15.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("vata faka");
+  }
+}
