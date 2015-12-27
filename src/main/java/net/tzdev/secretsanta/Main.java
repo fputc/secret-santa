aeca79eeb49af26ba0baf21d703808d58ad11262
fputc <alen.helac@gmail.com>
2015-12-27 20:10:45 +0100

    added log4j

--- src/main/java/net/tzdev/secretsanta/Main.java
+++ src/main/java/net/tzdev/secretsanta/Main.java
@@ -1,11 +1,29 @@
 package net.tzdev.secretsanta;
 
+
+import org.apache.log4j.Logger;
+
 /**
  * Created by ahelac on 27-Dec-15.
  */
 public class Main {
 
+  private final static Logger logger = Logger.getLogger(String.valueOf(Main.class));
+
   public static void main(String[] args) {
     System.out.println("vata faka");
+
+    if(logger.isDebugEnabled()){
+      logger.debug("This is debug");
+    }
+
+    if(logger.isInfoEnabled()){
+      logger.info("This is info");
+    }
+
+    logger.warn("This is warn");
+    logger.error("This is error");
+    logger.fatal("This is fatal");
+
   }
 }
